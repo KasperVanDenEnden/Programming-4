b3e4d58f6234faecc62a6cbfa8005564a7ed91dd
Kasper van den Enden <94372848+KasperVanDenEnden@users.noreply.github.com>
2022-04-24 10:00:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,88 @@
-const http = require('http')
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const port = process.env.PORT || 3000
-const result = {
-  code: 200,
-  message: "Hello darkness"
-}
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const res = require('express/lib/response');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end(JSON.stringify(result))
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
 })
 
-server.listen(port, () => {
-  console.log(`Server running at http://${port}/`)
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!")
 })
+
+module.exports = app;
+
+// set responds
+app.get("/user", (req,res) => {
+  console.log(req)
+  res.send("Got a GET all users request")
+})
+
+app.get("/user/{id}", (req,res) => {
+  console.log(req)
+  res.send("Got a GET an user request")
+})
+
+app.post("/", (req,res) => {
+  console.log(req)
+  res.send("Got a POST request")
+})
+
+app.put("/user", (req,res) => {
+  console.log(req)
+  res.send("Got a PUT request as /user")
+})
+
+app.delete("/user/{id}", (req,res) => {
+  console.log(req)
+  res.send("Got a DELETE request as /user")
+})
+
+app.put("/api/user", (req,res) => {
+  console.log(req)
+  res.send("Got a Create user request")
+})
+
+// serving static files in express
+express.static(root, [options])
+app.use(express.static('public'))
+app.use(express.static('files'))
+app.use("/static", express.static('public'))
