d1735e4dbe42077d80bc231930f5033e892c72c6
KasperVanDenEnden <kaspervde1996@hotmail.com>
2022-04-26 12:40:46 +0200

    testing added

---
+++ test/integration/user.controller.test.js
@@ -0,0 +1,36 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http');
+const res = require('express/lib/response');
+const server = require('../../index')
+let database = [];
+
+chai.should()
+chai.use(chaiHttp)
+
+describe('UC-201 Manage users ', () => {
+    describe('TC-1 add user /api/user',() =>{
+        beforeEach((done) => {
+            database = []
+            done()
+        })
+
+        it('TC-201 When a required input is missing, a valid error should be returned',() => {
+            chai
+            .request(server)
+            .post('/api/user')
+            .send({
+                // firstname ontbreekt
+                lastName: 'van den Enden',
+                phoneNumber: 06363,
+            })
+            .end((err,res) =>{
+                res.should.be.an('object')
+                let {status,result} = res.body
+                status.should.equals(400)
+                result.should.be.a('string').that.equals('Firstname must be a string')
+            })
+            done()
+        })
+    })
+    
+})
