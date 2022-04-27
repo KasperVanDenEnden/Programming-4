96ba0ad01cfb015de06e2833cf0f91e4a46c48fd
KasperVanDenEnden <kaspervde1996@hotmail.com>
2022-04-27 13:10:49 +0200

    Meal path files added

---
+++ src/routes/meal.routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const mealController = require("../controllers/meal.controller");
+
+// Meal End-points
+// create a meal
+router.post("/api/meal", mealController.addMeal);
+
+// get all meals
+router.get("/api/meal", mealController.getAllMeals);
+
+// get meal by id
+router.get("/api/meal/:id", mealController.getMealById);
+
+module.exports = router;
